d325413411a05d3048654663c1b136d6d0b73288
Prak-Pisith <pisithcreate@gmail.com>
2024-04-25 15:11:08 +0700

    Updated ApplicationConfiguration

--- src/main/java/pisith/xyz/securityjwtv2/configs/ApplicationConfiguration.java
+++ src/main/java/pisith/xyz/securityjwtv2/configs/ApplicationConfiguration.java
@@ -3,9 +3,15 @@ package pisith.xyz.securityjwtv2.configs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import pisith.xyz.securityjwtv2.users.UserRepository;
 
 // To Override the UserDetailsService
@@ -21,5 +27,23 @@ public class ApplicationConfiguration {
     public UserDetailsService userDetailsService() {
         return (UserDetailsService) username -> userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Username Not Found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
