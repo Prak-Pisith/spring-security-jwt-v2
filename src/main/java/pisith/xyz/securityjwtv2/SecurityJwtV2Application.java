091f46f49173a81f0aa2262b2399271194d26889
Prak-Pisith <pisithcreate@gmail.com>
2024-04-25 10:33:59 +0700

    Initial commit

---
+++ src/main/java/pisith/xyz/securityjwtv2/SecurityJwtV2Application.java
@@ -0,0 +1,13 @@
+package pisith.xyz.securityjwtv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pisith/xyz/securityjwtv2/SecurityJwtV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pisith.xyz.securityjwtv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
